b9089c70cb3254432dd268b6af3beb56fb5dcf0a
jsk900 <jsk900@yahoo.co.uk>
2019-09-25 10:18:43 +0200

    Todo-App

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import { Router } from './components/Router';
+
+ReactDOM.render(<Router />, document.querySelector('#root'));
